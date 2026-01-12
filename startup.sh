1ad32a31ccf83bfa17ea550b255bf439e6249388
AtliTyr <akulibabaev2.0@mail.ru>
2026-01-12 17:19:40 +0300

    скрипт

--- startup.sh
+++ startup.sh
@@ -2,13 +2,14 @@
 set -e  # остановить скрипт при любой ошибке
 
 echo "Waiting for database connection..."
+sleep 5
 
 # Опционально: добавить проверку подключения к БД перед миграциями
-while ! pg_isready -h $(echo $DATABASE_URL | sed -E 's/.*@([^:]+):.*/\1/') -p $(echo $DATABASE_URL | sed -E 's/.*:([0-9]+)\/.*/\1/') >/dev/null 2>&1; do
-  echo "Database not ready yet, waiting..."
-  echo $DATABASE_URL
-  sleep 2
-done
+# while ! pg_isready -h $(echo $DATABASE_URL | sed -E 's/.*@([^:]+):.*/\1/') -p $(echo $DATABASE_URL | sed -E 's/.*:([0-9]+)\/.*/\1/') >/dev/null 2>&1; do
+#   echo "Database not ready yet, waiting..."
+#   echo $DATABASE_URL
+#   sleep 2
+# done
 
 echo "Database is ready. Running migrations..."
 alembic upgrade head
